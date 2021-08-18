056bb068d629719e5f133479bda5d67474f91ab9
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-08-18 19:51:10 +0200

    install nuxt prop decorator. create base header file. use class based api for now.

---
+++ components/BaseHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <!--
+    @author: Pierre Dahmani
+    @created: 18.08.2021
+    @file: Default header for this app
+-->
+    <div>
+        <h1>hello world</h1>
+        <p>how are you doing?</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style scoped></style>
+
+
