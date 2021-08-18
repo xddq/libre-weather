f946a9106a98ab9ef57eb3ae84f9c46904baf493
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-08-18 20:45:11 +0200

    why prettier not running on save?

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,7 @@
 <template>
     <div>
         <div
-            class="
-                flex flex-col
-                items-center
-                justify-center
-                w-full
-                mt-4
-                location-input
-            "
+            class="flex flex-col items-center justify-center w-full mt-4 location-input"
         >
             <input type="text" placeholder="enter your city here" />
             <button class="send-location-request">Get weather!</button>
