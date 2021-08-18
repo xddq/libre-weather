97ad445b7dc66d6033b030f0ce75f052bb7e14e9
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-08-18 22:34:38 +0200

    install correct eslint packages.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    // prettier last to overwrite all format rules that could mess up pretter
-    "plugin:prettier/recommended",
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: [
+        "@nuxtjs/eslint-config-typescript",
+        "plugin:nuxt/recommended",
+        // prettier last to overwrite all format rules that could mess up pretter
+        "plugin:prettier/recommended",
+    ],
+    plugins: [],
+    // add your custom rules here
+    rules: {},
+};
