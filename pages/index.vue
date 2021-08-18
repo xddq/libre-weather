05421308c302ac3df07b460fca1afe1fe11a0520
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-08-18 21:08:38 +0200

    why again problems with format on save? :(

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,50 @@
 <template>
     <div>
         <div
-            class="flex flex-col items-center justify-center w-full mt-4 location-input"
+            class="
+                flex
+                items-center
+                justify-center
+                w-full
+                mt-4
+                location-input
+                space-x-2
+            "
         >
-            <input type="text" placeholder="enter your city here" />
-            <button class="send-location-request">Get weather!</button>
+            <input
+                class="
+                    text-center
+                    border border-solid
+                    w-80
+                    border-grey-300
+                    rounded-xl
+                "
+                type="text"
+                placeholder="enter your city here"
+            />
+            <button
+                class="
+                    send-location-request
+                    items-center
+                    border border-solid
+                    bg-gray-100
+                    rounded-xl
+                    w-40
+                "
+            >
+                Get weather!
+            </button>
         </div>
 
-        <div class="weather-card">
-            <div class="results">
-                Results:
-                <pre>test.</pre>
-            </div>
+        class="border border-red-300 w-96"
+        <div class="results">
+            Results:
+            <pre>test.</pre>
         </div>
     </div>
+</template>
 
+<script lang="ts">
 import Vue from "vue";
 export default Vue.extend({});
+</script>
